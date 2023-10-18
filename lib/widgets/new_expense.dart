3c98ad3437218928674a86090cbe26b58d3fe8ad
kingn <yasseralhassan942@gmail.com>
2023-10-18 21:31:06 +0300

    add Date Picker

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -10,6 +10,11 @@ class NewExpense extends StatefulWidget {
 class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
+
+  void _presentDatePicker(){
+    showDatePicker(context: context, initialDate: DateTime.now(), firstDate: DateTime(2021), lastDate: DateTime.now());
+  }
+
   @override
   void dispose() {
     _titleController.dispose();
@@ -25,17 +30,39 @@ class _NewExpenseState extends State<NewExpense> {
           controller: _titleController,
           decoration: const InputDecoration(labelText: 'Title'),
         ),
-        TextField(
-          controller: _amountController,
-          keyboardType: TextInputType.number,
-          decoration: const InputDecoration(
-              prefixText: '\$',
-              labelText: 'Amount'),
+        Row(
+
+          children: [
+            Expanded(
+              child: TextField(
+                controller: _amountController,
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(
+                    prefixText: '\$',
+                    labelText: 'Amount'),
+              ),
+            ),
+            const SizedBox(width: 16),
+            Expanded(
+              child: Row(
+                mainAxisAlignment:MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Text("Select Date"),
+                  IconButton(onPressed: _presentDatePicker,
+                    icon:const Icon(Icons.calendar_month),
+                  ),
+                ],
+              ),
+            )
+          ],
         ),
         const SizedBox(height: 16),
         Row(
           children: [
-            ElevatedButton(onPressed: (){}, child: Text('Close')),
+            ElevatedButton(onPressed: (){
+              Navigator.pop(context);
+            }, child: Text('Close')),
             const SizedBox(width: 16),
             ElevatedButton(onPressed:(){
               print(_titleController.text);
